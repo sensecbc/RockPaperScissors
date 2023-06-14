c4a1334d6d84ddd982c179a2bd0bd87ffb0d744d
sensecbc <rtavares4@ua.pt>
2023-06-14 18:13:37 +0100

    added some comments

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// As opções são rock paper e scissors
+
 const options = ["rock", "paper", "scissors"];
+
+// Esta função vai buscar uma das opções em cima a partir da options.lenght (0=rock, 1=paper e 2=scissors), vai multiplar por um número entre 0 e 1 e com esse resultado vamos obter, de forma aleatória, rock paper ou scissors com a ajudar do math.floor que basicamente arredonda para 0, 1 ou 2 em vez de ficar decimal
 
 function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
+
+// Esta função checka quem é o vencedor. Damos os parâmetros playerSelection e computerSelection e dizemos que caso o playerSelection seja igual a computerSelection então o resultado é tie, se playerSelection vencer computerSelection então o resultado é vitor e se o computerSelection vencer o playerSelection então é loss
+// Atenção que aqui ainda não dissemos o que são o playerSelection e o computerSelection, ainda não lhes foi associado nada.
 
 function checkWinner(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
@@ -21,6 +28,8 @@ function checkWinner(playerSelection, computerSelection){
     }
 }
 
+// Nesta função, com aquilo que fizemos anteriormente, vamos basicamente associar os resultados a algo que o utilizador possa realmente ver. Começamos por dar à variável result a função checkWinner e basicamente dizemos que caso essa variável seja igual a empate então o utilizador irá receber feedback a dizer que foi empate, caso seja vitória  então o utilizador recebe vitória e  acso seja derrota então o utilizador recebe derrota
+
 function playRound(playerSelection, computerSelection){
     const result = checkWinner(playerSelection, computerSelection);
     if (result == "tie"){
@@ -34,6 +43,8 @@ function playRound(playerSelection, computerSelection){
         return `you've lost because ${computerSelection} beats ${playerSelection}`
     }
 }
+
+// Nesta função vamos converter o texto em minúsculas de maneira a que o utilizador seja obrigado a usar "rock", "paper", "scissors". (ainda n percebo mt bem..)
 
 function getPlayerChoice(){
     let validatedInput = false;
@@ -50,6 +61,8 @@ function getPlayerChoice(){
     }
 
 }
+
+// Nesta última função, é basicamente quando começamos o jogo. Dizemos que tanto o computador como o utilizador começam com o score a 0, iniciamos um for loop em que quando chegar a 5 o jogo acaba, damos finalmente valor ao computerSelection e ao playerSelection e dizemos que caso o playerselection ganhe então adiciona um ponto ao player e vice-versa. No final basicamente estamos a mostrar ao utilizador se perdeu, ganhou ou empatou.
 
 function game(){
     let scorePlayer = 0;
